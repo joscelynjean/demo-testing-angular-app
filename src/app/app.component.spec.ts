05d8bf9f9e105d781922207e64e903781ed875c3
JoscelynJean <joscelyn.jean@ville.montreal.qc.ca>
2019-04-25 14:06:08 -0400

    Display player list and unit testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,97 @@
-import { TestBed, async } from '@angular/core/testing';
+import { PlayersService } from './players.service';
+import { TestBed, async, tick, fakeAsync, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Player } from './player';
+import { Observable, of, Scheduler, asyncScheduler } from 'rxjs';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+
+  const playersServiceStub = {
+    getList(): Observable<Player[]> {
+      const players: Player[] = [{
+        jerseyNumber: 40,
+        player: 'Joel Armia\\armiajo01',
+        flag: 'fi',
+        position: 'RW',
+        age: 25,
+        heigth: '6-4',
+        weight: 210,
+        experience: '4',
+        birthDate: 'May 31 1993',
+        summary: '13 G 10 A 23 P',
+        salary: '$1850000',
+        draft: '2011 BUF 1st (16)'
+      },
+      {
+        jerseyNumber: 40,
+        player: 'Joel Armia\\armiajo01',
+        flag: 'fi',
+        position: 'RW',
+        age: 25,
+        heigth: '6-4',
+        weight: 210,
+        experience: '4',
+        birthDate: 'May 31 1993',
+        summary: '13 G 10 A 23 P',
+        salary: '$1850000',
+        draft: '2011 BUF 1st (16)'
+      }
+      ];
+      return of(players);
+    }
+  };
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let playersService: PlayersService;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [{
+        provide: PlayersService, useValue: playersServiceStub
+      }]
     }).compileComponents();
-  }));
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    playersService = TestBed.get(PlayersService);
+
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo-testing-angular-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have as title 'List of hockey players!'`, () => {
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('demo-testing-angular-app');
+    expect(app.title).toEqual('List of hockey players!');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demo-testing-angular-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('List of hockey players!');
   });
+
+  it('should retrieve a list of hockey players', () => {
+    expect(component.players.length).toEqual(0);
+    spyOn(component, 'ngOnInit').and.callThrough(); // Call through will delegate the function call.
+
+    fixture.detectChanges();  // Trigger ngOnInit
+    expect(component.ngOnInit).toHaveBeenCalled();
+    expect(component.players.length).toEqual(2);
+  });
+
 });
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Player } from './player';
+import { PlayersService } from './players.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'demo-testing-angular-app';
+export class AppComponent implements OnInit {
+  title = 'List of hockey players!';
+  players: Player[] = [];
+
+  constructor(private playersService: PlayersService) { }
+
+  public ngOnInit(): void {
+    // Get players
+    this.playersService.getList().subscribe(
+      listOfPlayers => {
+        this.players = listOfPlayers;
+      }
+    );
+  }
+
 }
