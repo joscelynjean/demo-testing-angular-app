74b6632b21fa1d0c9f692213d23fa446c4200f60
JoscelynJean <joscelyn.jean@ville.montreal.qc.ca>
2019-04-29 10:49:35 -0400

    Start and stop mock API for e2e testing purpose

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -1,10 +1,22 @@
 const config = require('./protractor.conf').config;
 
+let jsonServerProcess;
+
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
     args: ['--headless', '--no-sandbox']
   }
 };
+
+// Allow startup of json-server as a mock API
+config.beforeLaunch = () => {
+  jsonServerProcess = exec('npm run api');
+};
+
+// Stop the mock API
+config.onCleanUp = () => {
+  process.kill(jsonServerProcess.pid);
+}
 
 exports.config = config;
