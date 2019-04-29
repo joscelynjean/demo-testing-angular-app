037dfc92626978412eee8a354fde89702fdf7bc6
JoscelynJean <joscelyn.jean@ville.montreal.qc.ca>
2019-04-29 11:11:15 -0400

    Add script to handle api start

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -9,14 +9,4 @@ config.capabilities = {
   }
 };
 
-// Allow startup of json-server as a mock API
-config.beforeLaunch = () => {
-  jsonServerProcess = exec('npm run api');
-};
-
-// Stop the mock API
-config.onCleanUp = () => {
-  process.kill(jsonServerProcess.pid);
-}
-
 exports.config = config;
